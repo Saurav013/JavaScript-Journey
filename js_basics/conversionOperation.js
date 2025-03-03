6a9f559f1ed72ea2691b76c1b852249e0bd36df0
Srv_x13 <krsaurav1326@gmail.com>
2025-03-03 11:58:15 +0530

    Update file

--- js_basics/conversionOperation.js
+++ js_basics/conversionOperation.js
@@ -2,7 +2,7 @@
 
 // Conversion of Number to String
 let score = "33abc"
-
+/*
 console.log(typeof score);
 console.log(typeof(score));
 
@@ -26,3 +26,46 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+*/ 
+
+// ********************** OPERATIONS ************************************
+
+let value = 3;
+let negValue = -value
+
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+// console.log(2/3);
+
+// Adding two string
+
+let str1 = "Hello";
+let str2 = "Saurav";
+
+let str3  = str1 + str2;
+// console.log(str3);
+console.log(str1 + " " + str2);
+
+// console.log(1 + 1);
+// console.log("1" + 1);
+// console.log(1 + "1");
+// console.log(1 + 1 + 1)
+// console.log(1 + "1" + 2);
+// console.log(1 + 2 + "3");
+
+let x = 3;
+const y = x++;
+console.log("x : ",x)
+console.log("y : ",y)
+
+let a = 3;
+const b = ++a;
+console.log("a : ",a);
+console.log("b : ",b);
+
+
