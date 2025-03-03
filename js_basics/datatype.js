437ca13bb0831b4f3fa35590276f8cba43ddec2b
Srv_x13 <krsaurav1326@gmail.com>
2025-03-03 10:05:45 +0530

    Data type of JavaScript

---
+++ js_basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict"  // treat all JS code as newer version
+
+// Data Types in JavaScript
+
+// (i) Number or bigInt
+let num = 5
+console.log(typeof(num))
+console.log(typeof num)
+
+// (ii) string -> " "
+let name = "Saurav"
+console.log(typeof(name))
+
+// (iii) boolean -> true/false
+let isLoggedIn = true
+console.log(typeof(isLoggedIn))
+
+// (iv) null -> standalone value
+let state = null
+console.log(typeof(state))
+
+
+// (v) undefined 
+// (vi) Object
+
+
