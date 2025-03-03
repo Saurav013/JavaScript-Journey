7e283ae0d043c25cf0ddaeeddf4e5af8c5a80d58
Srv_x13 <krsaurav1326@gmail.com>
2025-03-03 10:50:28 +0530

    Modifying in datatype.js

--- js_basics/datatype.js
+++ js_basics/datatype.js
@@ -1,6 +1,7 @@
 "use strict"  // treat all JS code as newer version
 
 // Data Types in JavaScript
+// (A) Primitive Datatype
 
 // (i) Number or bigInt
 let num = 5
@@ -23,4 +24,43 @@ console.log(typeof(state))
 // (v) undefined 
 // (vi) Object
 
+let big = BigInt(123)
+console.log(big)
 
+// (B) Non-Primitive Data type
+       // (i) Object
+
+       const stdinfo = {
+        Name : 'Saurav',
+        uid : 5616,
+        course : 'Btech',
+        pass_or_fail : true
+       }
+       console.log(stdinfo)
+       console.log(typeof stdinfo)
+       // (ii) Array
+       // (iii) function
+
+// Operator
+
+let val = 10;
+val += 45
+console.log(val)
+
+let count = 4
+console.log("Count :",count)
+
+count--
+console.log("Now Count : ",count)
+
+
+let age = 18;
+if(age >= 18){
+    console.log("Eligible for Voting")
+}else{
+    console.log("Not Eligible for the voting")
+}
+
+
+let check = 4 === '4'
+console.log(check)
